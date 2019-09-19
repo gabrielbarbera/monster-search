81be56c71c61f1d7e5b7b139bfb810ea750e737d
Gabriel Barberà (External) <gabriel.barbera@odigeo.org>
2019-09-19 15:57:02 +0200

    Updates

---
+++ precache-manifest.844bd8605af0737fc7bcc37f08d9bdfb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84df62b2c1cdad4426a7ebbfa50b0e11",
+    "url": "/monster-search/index.html"
+  },
+  {
+    "revision": "8aa7023b12370bc3c4f7",
+    "url": "/monster-search/static/css/main.db4899f8.chunk.css"
+  },
+  {
+    "revision": "3728ab537340a50308a2",
+    "url": "/monster-search/static/js/2.7c82c8af.chunk.js"
+  },
+  {
+    "revision": "8aa7023b12370bc3c4f7",
+    "url": "/monster-search/static/js/main.bb83c0de.chunk.js"
+  },
+  {
+    "revision": "5289d980033a74dbc90b",
+    "url": "/monster-search/static/js/runtime~main.2f61dd6f.js"
+  }
+]);
